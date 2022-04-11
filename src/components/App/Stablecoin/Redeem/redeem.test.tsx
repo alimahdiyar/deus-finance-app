abb23aa9620b30dfb02f42c82b94322ce5280f27
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-11 22:11:32 +0430

    mock image files in jest

--- src/components/App/Stablecoin/Redeem/redeem.test.tsx
+++ src/components/App/Stablecoin/Redeem/redeem.test.tsx
@@ -1,9 +1,12 @@
+import { Redeem } from 'components/App/Stablecoin'
+import { render } from '@testing-library/react'
+
 describe('Home', () => {
   it('renders a heading', () => {
-    // render(<Redeem />)
-    //
-    // const heading = screen.getByText('Claim')
-    //
-    // expect(heading).toBeInTheDocument()
+    render(<Redeem />)
+
+    const heading = screen.getByText('Claim')
+
+    expect(heading).toBeInTheDocument()
   })
 })
